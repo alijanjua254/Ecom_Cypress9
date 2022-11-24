fe996ee6cca9d6bf31b94c8dcbbb14b6bd4a2a42
Ali Janjua <muhammad.ali@ibcos.co.uk>
2022-11-24 16:37:57 +0500

    page object model implemented

--- cypress/integration/2-advanced-examples/Registration/registration.js
+++ cypress/integration/2-advanced-examples/Registration/registration.js
@@ -1,11 +1,14 @@
-
+import pageObjectRegister from "/cypress/fixtures/page_objects/register.json"
+import testDataRegister from "/cypress/fixtures/test_data/register.json"
+import pageObjectDashboard from"/cypress/fixtures/page_objects/dashboard.json"
 let global_email = "email" + Math.floor(Math.random()*1000 )+ "@gmail.com";
 
 
 describe('registration functionality', ()=>{
     it('tc03 Verify user is able to access register page', ()=>{
         cy.visit('https://demowebshop.tricentis.com/')
-        cy.get('.ico-register').click()
+        //using page object model i.e. dynamic locators through fixtures/page_objects/dashboard.json
+        cy.get(pageObjectDashboard.btn_register).click()
         cy.get('h1').should('be.visible')
 
     })
@@ -13,13 +16,14 @@ describe('registration functionality', ()=>{
         cy.get('#gender-male').click()
         cy.get('#gender-male').should('be.checked')
     })
-    it('tc05 Click register with incorrect password confirmation', ()=>{
-        cy.get('#FirstName').type('Ila', {delay : 100})
-        cy.get('#LastName').type('Ila', {delay : 100})
-        cy.get('#Email').type(global_email)
-        cy.get('#Password').type('aliila')
-        cy.get('#ConfirmPassword').type('aliil',{delay : 100})
-        cy.get('#register-button').click()
+    it('tc05 Click register with incorrect password confirmation WITH FIXTURES', ()=>{
+        //using page object model i.e. dynamic locators through fixtures/page_objects/register.json
+        cy.get(pageObjectRegister.field_FirstName).type('Ila', {delay : 100})
+        cy.get(pageObjectRegister.field_LastName).type('Ila', {delay : 100})
+        cy.get(pageObjectRegister.field_Email).type(global_email)
+        cy.get(pageObjectRegister.field_Password).type(testDataRegister.valid_password)
+        cy.get(pageObjectRegister.field_CnfmPassword).type('aliilaaa ',{delay : 100})
+        cy.get(pageObjectRegister.btn_registration).click()
         //assert incorrect confirm password validation
         cy.get('.field-validation-error > span').should('be.visible').should('have.text', 'The password and confirmation password do not match.')
     })
@@ -27,8 +31,9 @@ describe('registration functionality', ()=>{
         cy.get('#FirstName').clear().type('Ila', {delay: 100})
         cy.get('#LastName').clear().type('Ila', {delay: 100})
         cy.get('#Email').clear().type('aliila@gmail.com')
-        cy.get('#Password').clear().type('aliila')
-        cy.get('#ConfirmPassword').clear().type('aliila', {delay: 100})
+        //using test data i.e. dynamic data through fixtures/test_data/register.json
+        cy.get('#Password').clear().type(testDataRegister.valid_password)
+        cy.get('#ConfirmPassword').clear().type(testDataRegister.valid_cnfm_password, {delay: 100})
         cy.get('#register-button').click()
         cy.go('back')
         // This website behaves differently when automated through cypress and when you use it manually
